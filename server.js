d0898a9248977cac10c2b89e55d6a07fa7066e75
IdamEniIdam <idameni89@gmail.com>
2018-11-30 00:44:51 +1400

    Adding 'access control' to the app

--- server.js
+++ server.js
@@ -46,6 +46,18 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://idsoftconnector.herokuapp.com.herokuapp.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
