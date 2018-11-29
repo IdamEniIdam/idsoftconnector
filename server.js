34ea5a7242b8d80eca8523d60b91eeecb9f7ce67
IdamEniIdam <idameni89@gmail.com>
2018-11-30 04:42:38 +1400

    My App Prepared for deploy

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://idsoftconnector.herokuapp.com.herokuapp.com"
+    "https://idsoftconnector.herokuapp.com.herokuapp.com"
   );
   res.header(
     "Access-Control-Allow-Headers",
