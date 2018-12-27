945be9e9af00bf401f99d6c7def3f1c8241472bc
IdamEniIdam <idameni89@gmail.com>
2018-12-28 06:41:55 +1400

    added ussd code to the app

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
+const logger = require("morgan");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+app.use(logger("dev"));
 
 //Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -62,6 +65,41 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
+});
+
+app.post("*", (req, res) => {
+  let { sessionId, serviceCode, phoneNumber, text } = req.body;
+  if (text == "") {
+    // This is the first request. Note how we start the response with CON
+    let response = `CON What would you want to check
+    1. My Account
+    2. My phone number`;
+    res.send(response);
+  } else if (text == "1") {
+    // Business logic for first level response
+    let response = `CON Choose account information you want to view
+    1. Account number
+    2. Account balance`;
+    res.send(response);
+  } else if (text == "2") {
+    // Business logic for first level response
+    let response = `END Your phone number is ${phoneNumber}`;
+    res.send(response);
+  } else if (text == "1*1") {
+    // Business logic for first level response
+    let accountNumber = "ACC1001";
+    // This is a terminal request. Note how we start the response with END
+    let response = `END Your account number is ${accountNumber}`;
+    res.send(response);
+  } else if (text == "1*2") {
+    // This is a second level response where the user selected 1 in the first instance
+    let balance = "NGN 10,000";
+    // This is a terminal request. Note how we start the response with END
+    let response = `END Your balance is ${balance}`;
+    res.send(response);
+  } else {
+    res.status(400).send("Bad request!");
+  }
 });
 
 const port = process.env.PORT || 5000;
